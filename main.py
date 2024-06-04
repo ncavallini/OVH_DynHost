fd201b0af7b8af681f58a21cfaf11f311302b279
Niccolò Cavallini <73892369+ncavallini@users.noreply.github.com>
2024-06-04 11:05:42 +0200

    Update to use healthchecks.io

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import sys
 import requests
 from decouple import config
@@ -6,16 +7,18 @@ import dns
 import dns.resolver
 
 
+
+
 def update(hosts, username, password):
     ip = get_ip()
     for host in hosts:
         if get_current(host) == ip:
             print(f"[OK] {host} already up to date")
+            ping_healthchecks()
             continue
         res = requests.get(f"https://www.ovh.com/nic/update?system=dyndns&hostname={host}&myip={ip}", headers={"Authorization": auth_header(username, password)})
-        if res.status_code != 200: print(f"[ERROR] Failed to update {host} with {ip} - {res.text}")
-        else:
-            print(f"[OK] Updated {host} with {ip}")
+        if res.status_code == 200:
+            ping_healthchecks()
 
 def get_ip():
     return requests.get('https://api.ipify.org').text
@@ -25,9 +28,15 @@ def auth_header(username, password):
     bytes = raw.encode('ascii')
     return "Basic " + base64.b64encode(bytes).decode('ascii')
 
-def get_current(host):
+def get_current(host):   
     result = dns.resolver.resolve(host, 'A')
+    #print("CURR")
     return result[0].to_text()
+
+def ping_healthchecks():
+    url = "https://hc-ping.com/1e94ff38-c601-4085-880d-b62f7e3a70bb"
+    requests.post(url)
+
 
 if __name__ == '__main__':
     hosts = sys.argv[1:]
